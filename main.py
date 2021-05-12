e8f9dfd61c8c2ccbf5b41080cba8cc0968c508cb
Glitch <>
2021-05-12 19:50:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import os, random, time
+
+
+num = 0
+idk = 0
+
+'''
+user = os.environ["REPL_OWNER"]
+
+if user == "JBloves27":
+  delete = True
+else:
+  delete = False
+
+while delete:
+  idk+=1
+  if os.path.exists()
+'''
+
+while True:
+  WRDFILE = random.choice(["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"])
+  NUMFILE = str(random.randint(0, 9))
+  CHARFILE = random.choice(["`","~","!","@","#","$","%","%","^","&","*","(",")","-","_","=","+","[","{","]","}","\\","|",";",":","'","\"",",","<",".",">","/","?"])
+
+  NUMSFILE = random.randint(2, 27)
+
+  
+  open("","a")
